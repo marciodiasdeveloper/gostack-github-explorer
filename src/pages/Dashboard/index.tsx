e9582760f50f40a324d2e8a1838c52713722c1c0
Your Name <you@example.com>
2020-04-21 22:29:06 -0300

    Fix

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    console.log(response.data)
+  }
+
   return (
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-
-    <Repositories>
-      <a href="teste">
-        <img
-          alt="Márcio Dias"
-          src="https://avatars3.githubusercontent.com/u/75541?s=460&u=0d4944f37385b4904b096c050a50b53ae9e75ca1&v=4"
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>marciodiasdeveloper/portfolio</strong>
-          <p>description</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            alt="Márcio Dias"
+            src="https://avatars3.githubusercontent.com/u/75541?s=460&u=0d4944f37385b4904b096c050a50b53ae9e75ca1&v=4"
+          />
+          <div>
+            <strong>marciodiasdeveloper/portfolio</strong>
+            <p>description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
 
