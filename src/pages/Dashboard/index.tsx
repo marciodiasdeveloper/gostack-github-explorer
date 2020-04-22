fcf769f78812907247f3905be75082077b102fb8
Your Name <you@example.com>
2020-04-21 21:57:07 -0300

    Fix

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          alt="Márcio Dias"
+          src="https://avatars3.githubusercontent.com/u/75541?s=460&u=0d4944f37385b4904b096c050a50b53ae9e75ca1&v=4"
+        />
+        <div>
+          <strong>marciodiasdeveloper/portfolio</strong>
+          <p>description</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  );
 };
 
 export default Dashboard;
