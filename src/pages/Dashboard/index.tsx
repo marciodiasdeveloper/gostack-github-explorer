25187dbce22e3ab701e79bdf6f41af035999c432
Your Name <you@example.com>
2020-04-21 22:41:45 -0300

    Fix

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,23 +2,34 @@ import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
-
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+
 const Dashboard: React.FunctionComponent = () => {
   const [newRepo, setNewRepo] = useState('');
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
 
-    const response = await api.get(`repos/${newRepo}`);
+    const response = await api.get<Repository>(`repos/${newRepo}`);
 
-    console.log(response.data)
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   return (
@@ -36,17 +47,19 @@ const Dashboard: React.FunctionComponent = () => {
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            alt="Márcio Dias"
-            src="https://avatars3.githubusercontent.com/u/75541?s=460&u=0d4944f37385b4904b096c050a50b53ae9e75ca1&v=4"
-          />
-          <div>
-            <strong>marciodiasdeveloper/portfolio</strong>
-            <p>description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              alt={repository.owner.login}
+              src={repository.owner.avatar_url}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
